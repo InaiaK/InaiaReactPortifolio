393e94cda895f8eb7cca88b38c141afff74d5bcd
InaiaKenia <inaiakenia15@gmail.com>
2022-07-14 20:50:03 -0600

    updated about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -17,14 +17,11 @@ const styles = {
 export default function About() {
   return (
     <div style={styles.card}>
-      <h1 style={styles.heading}>About Page</h1>
+      <h1 style={styles.heading}>About </h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+     Hey there! I'm Inaia Martin.
+     I Am A Full Stack Student At University Of Denver. I Am Trilingual Professional ( Portuguese, English And Spanish) 
+      With Background And Experience In Hospitality/Tourism Industry.
       </p>
     </div>
   );
